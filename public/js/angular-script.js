1c8e0cdec3cc6d1f9299e73c762b0c9812827b96
Ankur Sarode <ankur.sarode@gmail.com>
2016-10-24 14:32:04 +0530

    Events From

--- public/js/angular-script.js
+++ public/js/angular-script.js
@@ -1,5 +1,7 @@
 var app = angular.module('app',[]);
 
 app.controller("mainController", function($scope){
-  $scope.x="12";
+  $scope.emailRegEx="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
+  $scope.regRegEx="^\\d{2}([A-Z]|[a-z]){3}\\d{4}$"
+
 });
