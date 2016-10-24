6d3901da175a5b001d99d526a284af8b07748867
Ankur Sarode <ankur.sarode@gmail.com>
2016-10-24 22:12:15 +0530

    Events Form Done

--- public/js/angular-script.js
+++ public/js/angular-script.js
@@ -1,7 +1,6 @@
 var app = angular.module('app',[]);
 
 app.controller("mainController", function($scope){
-  $scope.emailRegEx="/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/";
-  $scope.regRegEx="^\\d{2}([A-Z]|[a-z]){3}\\d{4}$"
-
+  $scope.regRegEx="^\\d{2}([A-Z]|[a-z]){3}\\d{4}$";
+  $scope.vitEmailRegEx="^([A-Z]|[a-z]){1,}(.){1}([A-Z]|[a-z]){1,}(@vit.ac.in)$"
 });
