ce6cda8e23935c426fa7a621e66232be00b0fa1d
AnkurSarode <ankur.sarode@gmail.com>
2016-10-30 02:27:32 +0530

    Event List Page

--- public/js/angular-script.js
+++ public/js/angular-script.js
@@ -4,3 +4,7 @@ app.controller("mainController", function($scope){
   $scope.regRegEx="^\\d{2}([A-Z]|[a-z]){3}\\d{4}$";
   $scope.vitEmailRegEx="^([A-Z]|[a-z]){1,}(.){1}([A-Z]|[a-z]){1,}(@vit.ac.in)$"
 });
+
+app.controller("listController", function($scope){
+  
+});
