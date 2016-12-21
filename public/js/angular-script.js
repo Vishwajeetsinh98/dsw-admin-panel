996e1d840db6b55b41ddb16b33ec595dc69a4fa8
AnkurSarode <ankur.sarode@gmail.com>
2016-12-21 19:44:20 +0530

    Edit Event

--- public/js/angular-script.js
+++ public/js/angular-script.js
@@ -6,6 +6,13 @@ app.controller("mainController", function($scope){
 });
 
 app.controller("listController", function($scope,$http){
+  $scope.formatDate=function(iDate){
+    var date=new Date(iDate);
+    var months=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var Fdate=date.getDate()+' '+months[date.getMonth()]+' '+date.getFullYear() ;
+    return Fdate;
+  }
+
   $scope.getEvents=function(data){
     $http.get('/events/list').success(function(data){
       $scope.events=data.data;
@@ -13,7 +20,136 @@ app.controller("listController", function($scope,$http){
   }
 });
 
-app.controller("eventController", function($scope,$http){
+app.controller("eventController", function($scope,$http,$filter,$location){
+  $scope.edit=false;
+
+  $scope.initEvents=function(evt,role){
+    $scope.role=role
+    $scope.id=evt._id;
+    $scope.startDate_model=$filter('date')(evt.startDate, 'yyyy-MM-dd');
+    $scope.endDate_model=$filter('date')(evt.endDate, 'yyyy-MM-dd');
+    $scope.startTime_model=$filter('date')(evt.startTime, 'HH:mm');
+    $scope.duration_model=evt.duration;
+    $scope.description_model=evt.eventDescription;
+    $scope.participants_model=evt.eventParticipants;
+    $scope.venue_model=evt.venueRequirement;
+    $scope.budget_model=evt.budget;
+    $scope.ceoRequest_model=evt.ceoRequest;
+    $scope.proRequest_model=evt.proRequest;
+    $scope.other_model=evt.other;
+  }
+
+  $scope.submit_form=function(){
+    if($scope.endDate_model<$scope.startDate_model){
+      alert("End Date Cannot be Smaller than Start Date");
+      return false;
+    }
+    data={}
+    count=1;
+    var V="value";
+    var F="field";
+    if($scope.editEventForm.startDate.$dirty){
+      ob={}
+      ob[F+count]='startDate'
+      ob[V+count]=$scope.startDate_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.endDate.$dirty){
+      ob={}
+      ob[F+count]='endDate'
+      ob[V+count]=$scope.endDate_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.startTime.$dirty){
+      ob={}
+      ob[F+count]='startTime'
+      ob[V+count]=$scope.startTime_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.duration.$dirty){
+      ob={}
+      ob[F+count]='duration'
+      ob[V+count]=$scope.duration_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.eventDescription.$dirty){
+      ob={}
+      ob[F+count]='eventDescription'
+      ob[V+count]=$scope.description_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.eventParticipants.$dirty){
+      ob={}
+      ob[F+count]='eventParticipants'
+      ob[V+count]=$scope.participants_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.venueRequirement.$dirty){
+      ob={}
+      ob[F+count]='venueRequirement'
+      ob[V+count]=$scope.venue_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.budget.$dirty){
+      ob={}
+      ob[F+count]='budget'
+      ob[V+count]=$scope.budget_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.ceoRequest.$dirty){
+      ob={}
+      ob[F+count]='ceoRequest'
+      ob[V+count]=$scope.ceoRequest_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.proRequest.$dirty){
+      ob={}
+      ob[F+count]='proRequest'
+      ob[V+count]=$scope.proRequest_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if($scope.editEventForm.other.$dirty){
+      ob={}
+      ob[F+count]='other'
+      ob[V+count]=$scope.other_model
+      data=Object.assign(data,ob);
+      count=count+1;
+    }
+    if(count==1){
+      vex.dialog.alert({
+          message: "No Changes Made"
+      })
+      return;
+    }
+    data["eventFor"]=$scope.id;
+    data["length"]=count-1;
+    if($scope.role=='dsw'){
+      $http.post('/dsw/editevent', data).success(function(data){
+        var route="/events/"+$scope.id;
+        window.location.href=route;
+      }).error(function(err){
+        console.log(err);
+      })
+    }else{
+      $http.post('/admin/editevent', data).success(function(data){
+        var route="/events/"+$scope.id;
+        window.location.href=route;
+      }).error(function(err){
+        console.log(err);
+      })
+    }
+  }
+
   $scope.admin_dsw=[];
   $scope.manage_dsw=function(admin_dsw){
   var i=$scope.admin_dsw.indexOf(admin_dsw);
@@ -33,7 +169,7 @@ app.controller("eventController", function($scope,$http){
       str=str+e+',';
     })
     str=str.slice(0, -1);
-    obj={'eventFor': $scope.id, 'roles': str}
+    obj={'eventId': $scope.id, 'roles': str}
     $http.post('/dsw/forward', obj).success(function(res){
       if(res){
         window.location.href="/home";
@@ -61,7 +197,7 @@ app.controller("eventController", function($scope,$http){
     })
     str=str.slice(0, -1);
     obj={'eventId': $scope.id, 'roles': str}
-    $http.post('/dsw/forward', obj).success(function(res){
+    $http.post('/admin/forward', obj).success(function(res){
       if(res){
         window.location.href="/home";
       }
