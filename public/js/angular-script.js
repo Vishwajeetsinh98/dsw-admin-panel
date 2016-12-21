23943116df6600fdf59a2a0c5a77e4c916347c8b
Ankur Sarode <ankur.sarode@gmail.com>
2016-12-21 14:46:48 +0530

    Filters

--- public/js/angular-script.js
+++ public/js/angular-script.js
@@ -12,8 +12,74 @@ app.controller("listController", function($scope,$http){
     });
   }
 });
-app.controller("allEventsController", function($scope,$http){
-  $scope.init=function(data){
-    $scope.events=data;
+
+app.filter("clubOrChapterFilter", function(){
+  return function(data, clubOrChapter){
+      var list=[];
+      if(clubOrChapter.length==0){
+        list=data;
+        return list;
+      }
+    data.forEach(function(e){
+      var i=clubOrChapter.indexOf(e.conductingBodyType);
+      if(i!=-1){
+        list.push(e);
+      }
+    })
+    return list;
+  }
+});
+
+app.filter("faApprovalFilter", function(){
+  return function(data, faApproval){
+      var list=[];
+      if(faApproval.length==0){
+        list=data;
+        return list;
+      }
+    data.forEach(function(e){
+      var i=faApproval.indexOf(e.fcApproval);
+      if(i!=-1){
+        list.push(e);
+      }
+    })
+    return list;
+  }
+});
+
+  app.filter("approvalFilter", function(){
+    return function(data, approval){
+        var list=[];
+        if(approval.length==0){
+          list=data;
+          return list;
+        }
+      data.forEach(function(e){
+        var i=approval.indexOf(e.approvalStatus);
+        if(i!=-1){
+          list.push(e);
+        }
+      })
+      return list;
+    }
+});
+
+app.filter("dateFilter", function(){
+  return function(data, startDate){
+    if(startDate==undefined){
+      return data;
+    }
+      var list=[];
+      startDate=new Date(startDate);
+      startDate=startDate.toString().substring(0, 15);
+      data.forEach(function(e){
+        date=new Date(e.startDate);
+        date=(date).toString().substring(0, 15);
+        console.log("sDate"+startDate+"\ndate:"+date)
+        if(date==startDate){
+          list.push(e);
+        }
+      })
+      return list;
   }
 });
